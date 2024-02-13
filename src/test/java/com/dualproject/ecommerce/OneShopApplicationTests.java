65803f06328034315899bd65e3ee0d18a811fb45
Franco Ferrari <franconferrari02@outlook.com>
2024-02-13 20:52:28 -0300

    Carga de proyecto Spring Boot

---
+++ src/test/java/com/dualproject/ecommerce/OneShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dualproject.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
